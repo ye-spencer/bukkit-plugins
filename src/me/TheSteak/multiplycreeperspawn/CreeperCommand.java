8a85d414e1b82e01a542bb8daddfe3e7d1c24699
Spencer Ye <sye348@student.fuhsd.org>
2020-12-17 21:29:25 -0800

    start coding a bit

--- src/me/TheSteak/multiplycreeperspawn/CreeperCommand.java
+++ src/me/TheSteak/multiplycreeperspawn/CreeperCommand.java
@@ -7,6 +7,14 @@ import org.bukkit.command.CommandSender;
 public class CreeperCommand implements CommandExecutor
 {
 
+	private Main plugin;
+	
+	public CreeperCommand(Main in)
+	{
+		this.plugin = in;
+		plugin.getCommand("creepspawn").setExecutor(this);
+	}
+	
 	@Override
 	public boolean onCommand(CommandSender arg0, Command arg1, String arg2, String[] arg3) 
 	{
