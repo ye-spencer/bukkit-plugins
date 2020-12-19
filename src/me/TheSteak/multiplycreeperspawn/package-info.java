66f1d22549da9e0d9370d9838f284a3a17a24f88
Spencer Ye <sye348@student.fuhsd.org>
2020-12-18 21:40:35 -0800

    Update package-info.java

--- src/me/TheSteak/multiplycreeperspawn/package-info.java
+++ src/me/TheSteak/multiplycreeperspawn/package-info.java
@@ -3,6 +3,8 @@
  */
 /**
  * @author TheSteak
- *	A package that contains the files that hopefully makes your day that much worse
+ *	A package that contains the files that hopefully makes your day that much worse (or better)
+ *
+ *
  */
 package me.TheSteak.multiplycreeperspawn;
