33288deabbf8858422a07432c7603bd6b5d0fd5b
Spencer Ye <sye348@student.fuhsd.org>
2020-12-26 22:10:35 -0800

    Update package-info.java

--- src/me/TheSteak/multiplycreeperspawn/package-info.java
+++ src/me/TheSteak/multiplycreeperspawn/package-info.java
@@ -1,6 +1,6 @@
 /**
  * @author TheSteak
- * @version 1.0.5
+ * @version 1.0.0
  *	A package that contains the files that hopefully makes your day that much worse (or better)
  *
  * A plugin that spawns between 1 and 64 creepers on the player 
