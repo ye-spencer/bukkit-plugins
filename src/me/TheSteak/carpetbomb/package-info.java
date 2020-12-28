33e66f650c5c1c804e8c5258669e34bf896eeb56
Spencer Ye <sye348@student.fuhsd.org>
2020-12-27 22:22:34 -0800

    update data

--- src/me/TheSteak/carpetbomb/package-info.java
+++ src/me/TheSteak/carpetbomb/package-info.java
@@ -1,1 +1,8 @@
+/**
+ * @author TheSteak
+ * @version 0.0.0
+ *
+ * A plugin that starts a carpet bomb 
+ * For Minecraft Java Version [n/a]
+ */
 package me.TheSteak.carpetbomb;
--- src/me/TheSteak/multiplycreeperspawn/package-info.java
+++ src/me/TheSteak/multiplycreeperspawn/package-info.java
@@ -1,9 +1,8 @@
 /**
  * @author TheSteak
  * @version 1.0.0
- *	A package that contains the files that hopefully makes your day that much worse (or better)
  *
  * A plugin that spawns between 1 and 64 creepers on the player 
- * For Minecraft Java Version 1.16.4
+ * For Minecraft Java Version 1.16.4, 1.8.8, 1.7.2
  */
 package me.TheSteak.multiplycreeperspawn;
