356fa817f075371bdec4b1f68fc8c11dec30b91d
Spencer Ye <sye348@student.fuhsd.org>
2021-01-02 21:16:29 -0800

    create new workspace

---
+++ src/me/TheSteak/multiteammanhunt/Main.java
@@ -0,0 +1,5 @@
+package me.TheSteak.multiteammanhunt;
+
+public class Main {
+
+}
---
+++ src/me/TheSteak/multiteammanhunt/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * A package that contains the files for my most recent plugin
+ */
+package me.TheSteak.multiteammanhunt;
