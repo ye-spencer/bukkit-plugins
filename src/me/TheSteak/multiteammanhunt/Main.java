872618d263854f3f6bd7a8f827dd7c2328ebf217
Spencer Ye <sye348@student.fuhsd.org>
2021-01-03 22:45:00 -0800

    start layout

--- src/me/TheSteak/multiteammanhunt/Main.java
+++ src/me/TheSteak/multiteammanhunt/Main.java
@@ -1,5 +1,20 @@
 package me.TheSteak.multiteammanhunt;
 
-public class Main {
+import org.bukkit.plugin.java.JavaPlugin;
 
+/*
+ * okay im not too sure on how these work, but i think once the plugin in loaded, it runs the main class, which creates all the other classes, which stay and wait for commands to be run,
+ * or anything else to happen
+ * 
+ * Location l=target.getLocation();
+player.setCompassTarget(l);
+ */
+
+public class Main extends JavaPlugin
+{
+	@Override
+	public void onEnable()
+	{
+		new MainhuntCompassCommand(this);
+	}
 }
