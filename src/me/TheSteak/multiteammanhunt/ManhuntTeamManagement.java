2a3f4479a7ae8d4819ffb4d20077bf8e45e86487
Spencer Ye <sye348@student.fuhsd.org>
2021-01-06 23:06:49 -0800

    add pointers

--- src/me/TheSteak/multiteammanhunt/ManhuntTeamManagement.java
+++ src/me/TheSteak/multiteammanhunt/ManhuntTeamManagement.java
@@ -6,13 +6,17 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-
+/*
+ * TODO:
+ * account for switching teams
+ */
 public class ManhuntTeamManagement implements CommandExecutor 
 {
 	
 	private Main plugin;
 	
 	ArrayList<Player> hunters, runners;
+	ArrayList<Integer> hunterpoint;
 	
 	public ManhuntTeamManagement (Main in)
 	{
@@ -22,6 +26,7 @@ public class ManhuntTeamManagement implements CommandExecutor
 		plugin.getCommand("switchtrack").setExecutor(this);
 		hunters = new ArrayList<Player>();
 		runners = new ArrayList<Player>();
+		hunterpoint = new ArrayList<Integer>();
 		
 	}
 
@@ -31,10 +36,25 @@ public class ManhuntTeamManagement implements CommandExecutor
 		if ("teamhunter".equals(cmd.getName()))
 		{
 			hunters.add((Player)sender);
+			if (runners.isEmpty())
+			{
+				hunterpoint.add(-1);
+			}
+			else
+			{
+				hunterpoint.add(0);
+			}
 		}
 		else if ("teamrunner".equals(cmd.getName()))
 		{
 			runners.add((Player)sender);
+			if (runners.size() == 1)
+			{
+				for (int i = 0; i < hunters.size(); ++i)
+				{
+					hunterpoint.set(i, 0);
+				}
+			}
 		}
 		else if ("switchtrack".equals(cmd.getName()))
 		{
