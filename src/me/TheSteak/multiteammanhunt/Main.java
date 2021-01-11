2c3c25198d7700ec04135456efce19d3d579b54f
Spencer Ye <sye348@student.fuhsd.org>
2021-01-11 13:23:20 -0800

    iDEA

--- src/me/TheSteak/multiteammanhunt/Main.java
+++ src/me/TheSteak/multiteammanhunt/Main.java
@@ -6,8 +6,6 @@ import org.bukkit.plugin.java.JavaPlugin;
  * okay im not too sure on how these work, but i think once the plugin in loaded, it runs the main class, which creates all the other classes, which stay and wait for commands to be run,
  * or anything else to happen
  * 
- * Location l=target.getLocation();
-player.setCompassTarget(l);
  */
 
 public class Main extends JavaPlugin
