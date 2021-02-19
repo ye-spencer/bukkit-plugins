fdeafc5984eb5201a974eb27a9e4c46c1fd67a0d
Spencer Ye <57966319+Concerned-Baby@users.noreply.github.com>
2021-02-18 17:01:21 -0800

    Create module-info.java

---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+module test {
+	exports me.TheSteak.carpetbomb;
+	exports me.TheSteak.footballgame;
+	exports me.TheSteak.multiplycreeperspawn;
+	exports me.TheSteak.multiteammanhunt;
+
+	requires java.logging;
+	requires spigot;
+}
