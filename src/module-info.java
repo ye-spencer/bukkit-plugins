20cdf8e497dc43ee914b413b9f945c918fbeb159
Don't Do Drugs <spencerye425@gmail.com>
2021-04-07 11:58:16 -0700

    Delete module-info.java

--- src/module-info.java
+++
@@ -1,9 +0,0 @@
-module test {
-	exports me.TheSteak.carpetbomb;
-	exports me.TheSteak.footballgame;
-	exports me.TheSteak.multiplycreeperspawn;
-	exports me.TheSteak.multiteammanhunt;
-
-	requires java.logging;
-	requires spigot;
-}
