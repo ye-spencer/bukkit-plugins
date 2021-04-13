96d168646ac057e690c03f79ada8f0f91ce66412
Don't Do Drugs <spencerye425@gmail.com>
2021-04-12 18:16:36 -0700

    remove comments

--- src/me/TheSteak/multiplycreeperspawn/Main.java
+++ src/me/TheSteak/multiplycreeperspawn/Main.java
@@ -2,11 +2,6 @@ package me.TheSteak.multiplycreeperspawn;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
-/*
-[21:42:37] [Server thread/WARN]: Legacy plugin CreeperSpawn v1.0.0 does not specify an api-version.
-[21:42:37] [Server thread/INFO]: [CreeperSpawn] Loading CreeperSpawn v1.0.0
-*/
-
 public class Main extends JavaPlugin
 {
 	@Override
