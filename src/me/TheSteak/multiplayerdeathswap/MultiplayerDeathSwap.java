aad4f8285cd08196cf7cb4be6c5ad5006821d2b4
Don't Do Drugs <spencerye425@gmail.com>
2021-04-15 17:14:53 -0700

    Update MultiplayerDeathSwap.java

--- src/me/TheSteak/multiplayerdeathswap/MultiplayerDeathSwap.java
+++ src/me/TheSteak/multiplayerdeathswap/MultiplayerDeathSwap.java
@@ -1,14 +1,19 @@
 package me.TheSteak.multiplayerdeathswap;
 
+import java.util.ArrayList;
+
 import org.bukkit.Server;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 public class MultiplayerDeathSwap implements CommandExecutor
 {
 	private Main plugin;
 	private Server server;
+	
+	private ArrayList<Player> players;
 	
 	public MultiplayerDeathSwap(Main main)
 	{
@@ -25,6 +30,18 @@ public class MultiplayerDeathSwap implements CommandExecutor
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] others) 
 	{
+		if ("joindeathgame".equals(cmd.getName()))
+		{
+			
+		}
+		else if ("leavedeathgame".equals(cmd.getName()))
+		{
+			
+		}
+		else if ("startdeathgame".equals(cmd.getName()))
+		{
+			
+		}
 		return false;
 	}
 }
