e0405a9461541eedce3d18a17432c97eff87d4e8
Concerned-Baby <sye348@student.fuhsd.org>
2021-06-28 11:57:49 -0700

    Update MultiplayerDeathSwap.java

--- src/me/TheSteak/multiplayerdeathswap/MultiplayerDeathSwap.java
+++ src/me/TheSteak/multiplayerdeathswap/MultiplayerDeathSwap.java
@@ -23,7 +23,7 @@ public class MultiplayerDeathSwap implements CommandExecutor, Listener
 	
 	private boolean gameStarted;
 	
-	private final int minutesVary = 3, minutesMin = 2, secondsPerCountdown = 10;
+	private final int minutesVary = 3, minutesMin = 2, secondsPerCountdown = 12;
 	
 	public MultiplayerDeathSwap(Main plugin)
 	{		
